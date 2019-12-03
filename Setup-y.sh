d7b904d353ce17164f0a05e2e03a199e70770236
MostafaElbordy <52612582+MostafaElbordy@users.noreply.github.com>
2019-12-03 23:46:18 +0200

    Add files via upload

---
+++ Setup-y.sh
@@ -0,0 +1,333 @@
+#Please Dont Take This Tool Without Putting Our Name
+#This Tool Was Maded By Mostafa Elbordy
+#This is the version 2 from Setter by Good Hacker
+#this tool will set up all packages -y
+#####################################################
+#colers
+a='\033[1;31m'
+b='\033[1;32m'
+c='\033[1;33m'
+d='\033[1;34m'
+e='\033[1;35m'
+f='\033[1;36m'
+g='\033[1;37m'
+h='\033[1;38m'
+i='\033[1;39m'
+j='\033[1;40m'
+k='\033[1;41m'
+l='\033[1;42m'
+m='\033[1;43m'
+n='\033[1;44m'
+clear
+
+start(){
+echo -e "$f  _____   _____       ___   _____    _____   _   __   _   _____  "
+sleep 0.2
+echo -e "$e /  ___/ |_   _|     /   | |  _  \  |_   _| | | |  \ | | /  ___| "
+sleep 0.2
+echo -e "$d | |___    | |      / /| | | |_| |    | |   | | |   \| | | |     "
+sleep 0.2
+echo -e "$c \___  \   | |     / / | | |  _  /    | |   | | | |\   | | |  _  "
+sleep 0.2
+echo -e "$b  ___| |   | |    / /  | | | | \ \    | |   | | | | \  | | |_| | "
+sleep 0.2
+echo -e "$a /_____/   |_|   /_/   |_| |_|  \_\   |_|   |_| |_|  \_| \_____/ "
+sleep 0.2
+}
+logo(){
+echo -e "$a _____   _____   _____   _____   _____   _____             __    __ "
+sleep 0.2
+echo -e "$b/  ___/ | ____| |_   _| |_   _| | ____| |  _  \            \ \  / / "
+sleep 0.2
+echo -e "$c| |___  | |__     | |     | |   | |__   | |_| |             \ \/ /  "
+sleep 0.2
+echo -e "$d\___  \ |  __|    | |     | |   |  __|  |  _  /   ========   \  /   "
+sleep 0.2
+echo -e "$e ___| | | |___    | |     | |   | |___  | | \ \              / /    "
+sleep 0.2
+echo -e "$f/_____/ |_____|   |_|     |_|   |_____| |_|  \_\            /_/     "
+sleep 0.2
+echo ''
+sleep 0.2
+echo -e "$a======================== By Mostafa Elbord ========================="
+sleep 0.2
+echo ''
+echo -e "$d================ install the first tool from this link ============="
+sleep 0.2
+echo ''
+echo -e "$c======= https://github.com/MostafaElbordy/SetterByGoodHacker ======="
+sleep 0.2
+echo ''
+echo -e "$g============== Setter -y  will install all packags -y =============="
+sleep 0.2
+echo ''
+}
+end(){
+echo -e "$g _____  __    __  _____  "
+sleep 0.2
+echo -e "$h|  _  \ \ \  / / | ____| "
+sleep 0.2
+echo -e "$a| |_| |  \ \/ /  | |__   "
+sleep 0.2
+echo -e "$b|  _  |   \  /   |  __|  "
+sleep 0.2
+echo -e "$c| |_| |   / /    | |___  "
+sleep 0.2
+echo -e "$d|_____/  /_/     |_____| "
+}
+instal(){
+echo -e "$f _   __   _   _____   _____       ___   _       _       _   __   _   _____  "
+sleep 0.2
+echo -e "$g| | |  \ | | /  ___/ |_   _|     /   | | |     | |     | | |  \ | | /  ___| "
+sleep 0.2
+echo -e "$h| | |   \| | | |___    | |      / /| | | |     | |     | | |   \| | | |     "
+sleep 0.2
+echo -e "$a| | | |\   | \___  \   | |     / / | | | |     | |     | | | |\   | | |  _  "
+sleep 0.2
+echo -e "$b| | | | \  |  ___| |   | |    / /  | | | |___  | |___  | | | | \  | | |_| | "
+sleep 0.2
+echo -e "$c|_| |_|  \_| /_____/   |_|   /_/   |_| |_____| |_____| |_| |_|  \_| \_____/ "
+}
+instaled(){
+echo -e  "$a_   __   _   _____   _____       ___   _       _       _____   _____  "
+sleep 0.2
+echo -e  "$b| | |  \ | | /  ___/ |_   _|     /   | | |     | |     | ____| |  _  \ "
+sleep 0.2
+echo -e  "$c| | |   \| | | |___    | |      / /| | | |     | |     | |__   | | | | "
+sleep 0.2
+echo -e  "$d| | | |\   | \___  \   | |     / / | | | |     | |     |  __|  | | | | "
+sleep 0.2
+echo -e  "$e| | | | \  |  ___| |   | |    / /  | | | |___  | |___  | |___  | |_| | "
+sleep 0.2
+echo -e  "$f|_| |_|  \_| /_____/   |_|   /_/   |_| |_____| |_____| |_____| |_____/ "
+}
+thanks(){
+echo -e "$a _____   _   _       ___   __   _   _   _    _____  "
+sleep 0.2
+echo -e "$b|_   _| | | | |     /   | |  \ | | | | / /  /  ___/ "
+sleep 0.2
+echo -e "$c  | |   | |_| |    / /| | |   \| | | |/ /   | |___  "
+sleep 0.2
+echo -e "$d  | |   |  _  |   / / | | | |\   | | |\ \   \___  \ "
+sleep 0.2
+echo -e "$e  | |   | | | |  / /  | | | | \  | | | \ \   ___| | "
+sleep 0.2
+echo -e "$f  |_|   |_| |_| /_/   |_| |_|  \_| |_|  \_\ /_____/ "
+}
+
+
+logo
+sleep 0.2
+echo -e "$f Do you want to start installing packags ?"
+echo ''
+echo -n -e " {$a 1 $a}";sleep 0.1;echo -n -e "$b Yes $b"
+echo ''
+echo -n -e " {$a 0 $a}";sleep 0.1;echo -n -e "$b No $b"
+echo ''
+echo -n -e "$h Yes or No ? ";read s;echo ''
+if [ $s -eq 1 ];then
+	clear
+	instal
+	sleep 0.2
+	clear
+	start
+	sleep 0.2
+	clear
+	pkg update -y
+	sleep 0.2
+	clear
+	pkg upgrade -y
+	sleep 0.2
+        clear
+	pkg install python -y
+	sleep 0.2
+        clear
+	pkg install python2 -y
+	sleep 0.2
+        clear
+	pkg install python2-dev -y
+	sleep 0.2
+        clear
+	pkg install python3 -y
+	sleep 0.2
+        clear
+	pkg install java -y
+	sleep 0.2
+        clear
+	pkg install fish -y
+	sleep 0.2
+        clear
+	pkg install ruby -y
+	sleep 0.2
+        clear
+	pkg install help -y
+	sleep 0.2
+        clear
+	pkg install git -y
+	sleep 0.2
+        clear
+	pkg install host -y
+	sleep 0.2
+        clear
+	pkg install php -y
+	sleep 0.2
+        clear
+	pkg install perl -y
+	sleep 0.2
+        clear
+	pkg install nmap -y
+	sleep 0.2
+        clear
+	pkg install bash -y
+	sleep 0.2
+        clear
+	pkg install clang -y
+	sleep 0.2
+        clear
+	pkg install nano -y
+	sleep 0.2
+        clear
+	pkg install w3m -y
+	sleep 0.2
+        clear
+	pkg install havij -y
+	sleep 0.2
+        clear
+	pkg install hydra -y
+	sleep 0.2
+        clear
+	pkg install figlet -y
+	sleep 0.2
+        clear
+	pkg install cowsay -y
+	sleep 0.2
+        clear
+	pkg install curl -y
+	sleep 0.2
+        clear
+	pkg install tar -y
+	sleep 0.2
+        clear
+	pkg install zip -y
+	sleep 0.2
+        clear
+	pkg install unzip -y
+	sleep 0.2
+        clear
+	pkg install tor -y
+	sleep 0.2
+        clear
+	pkg install google -y
+	sleep 0.2
+        clear
+	pkg install sudo -y
+	sleep 0.2
+        clear
+	pkg install wget -y
+	sleep 0.2
+        clear
+	pkg install wireshark -y
+	sleep 0.2
+        clear
+	pkg install wgetrc -y
+	sleep 0.2
+        clear
+	pkg install wcalc -y
+	sleep 0.2
+        clear
+	pkg install bmon -y
+	sleep 0.2
+        clear
+	kg install vpn -y
+	sleep 0.2
+        clear
+	pkg install unrar -y
+	sleep 0.2
+        clear
+	pkg install toilet -y
+	sleep 0.2
+        clear
+	pkg install proot -y
+	sleep 0.2
+        clear
+	pkg install net-tools -y
+	sleep 0.2
+        clear
+	pkg install golang -y
+	sleep 0.2
+        clear
+	pkg install chroot -y
+	sleep 0.2
+        clear
+	pkg install macchanger -y
+	sleep 0.2
+        clear
+	pkg install openssl -y
+	sleep 0.2
+        clear
+	pkg install cmatrix -y
+	sleep 0.2
+        clear
+	pkg install openssh -y
+	sleep 0.2
+        clear
+	pkg install wireshark -y
+	sleep 0.2
+        clear
+	pkg install macchanger -y
+	sleep 0.2
+        clear
+	apt update && apt upgrade -y
+	sleep 0.2
+        clear
+	pkg install tsu
+	sleep 0.2
+	clear
+	instaled
+	sleep 0.2
+	echo -e "$f Do you need to install metasploit ?"
+	sleep 0.2
+	echo ''
+	sleep 0.2
+	echo -n -e " {$a 1 $a}";sleep 0.1;echo -n -e "$b Yes $b"
+	sleep 0.2
+	echo ''
+	sleep 0.2
+	echo -n -e " {$a 0 $a}";sleep 0.1;echo -n -e "$b No $b"
+	sleep 0.2
+	echo ''
+	sleep 0.2
+	echo -n -e "$h Yes or No ? ";read k;echo ''
+	sleep 0.2
+	if [ $k -eq 1 ];then
+	instal
+	sleep 0.2
+	clear
+	start
+	sleep 0.2
+	clear
+	pkg install unstable-repo
+	sleep 0.2
+	pkg install metasploit
+	sleep 0.2
+	clear
+	fi
+	if [ $k -eq 0 ];then
+	clear
+	thanks
+	sleep 0.2
+	clear
+	end
+	sleep 0.2
+	clear
+	fi
+fi
+if [ $s -eq 0 ];then
+	clear
+	thanks
+	sleep 0.2
+	clear
+	end
+	sleep 0.2
+	clear
+fi
